0296d478945b291a0d1771dffc9511e28cd4fb7f
AchaUmbaPretre <achandambi@gmail.com>
2025-02-19 14:48:21 +0100

    commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,12 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import * as Location from 'expo-location'
 
 const HomeScreen = () => {
+    useEffect(()=> {
+        checkIfLocationEnabled();
+        getCurrentLocation();
+    }, [])
   return (
     <SafeAreaView>
       <Text>HomeScreen</Text>
